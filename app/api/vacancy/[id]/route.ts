a3e78657c803d86aa1398148b4955073caf86716
Vadim Shchigolev <113100090+varushchi@users.noreply.github.com>
2024-11-22 13:02:33 +0000

    awaited id in api

--- app/api/vacancy/[id]/route.ts
+++ app/api/vacancy/[id]/route.ts
@@ -1,20 +1,24 @@
-import connectMongoDB from '@/libs/mongodb'
-import Vacancy from '@/models/vacancy'
-import { NextResponse, NextRequest } from 'next/server'
+import connectMongoDB from "@/libs/mongodb";
+import Vacancy from "@/models/vacancy";
+import { NextResponse, NextRequest } from "next/server";
 
-
-
-export async function PUT(req: NextRequest, {params}: {params: {id: string}}){
-  const {id} = params
-  const vacancyData = await req.json()
-  await connectMongoDB()
-  await Vacancy.findByIdAndUpdate(id, vacancyData)
-  return NextResponse.json({message: 'vacancy updated'}, {status: 200})
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await params;
+  const vacancyData = await req.json();
+  await connectMongoDB();
+  await Vacancy.findByIdAndUpdate(id, vacancyData);
+  return NextResponse.json({ message: "vacancy updated" }, { status: 200 });
 }
 
-export async function GET(req: NextRequest, {params}: {params: {id: string}}){
-  const {id} = params
-  await connectMongoDB()
-  const vacancy = await Vacancy.findOne({_id: id})
-  return NextResponse.json({vacancy}, {status: 200})
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  await connectMongoDB();
+  const vacancy = await Vacancy.findOne({ _id: id });
+  return NextResponse.json({ vacancy }, { status: 200 });
 }
